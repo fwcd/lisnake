d8a3dd54fe9200de654f60aef7ec200a8f1251a4
fwcd <fwcd@fwcd.dev>
2025-03-06 01:38:09 +0100

    Vastly simplify input handling
    ...thanks to the new direction abstractions in lighthouse-rust

--- src/controller.rs
+++ src/controller.rs
@@ -2,40 +2,19 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use futures::{lock::Mutex, prelude::*, Stream};
-use lighthouse_client::protocol::{Delta, GamepadButtonEvent, GamepadControlEvent, GamepadEvent, InputEvent, KeyEvent, ServerMessage};
+use lighthouse_client::protocol::{InputEvent, ServerMessage};
 use tracing::debug;
 
 use crate::model::State;
 
 pub async fn run(mut stream: impl Stream<Item = lighthouse_client::Result<ServerMessage<InputEvent>>> + Unpin, shared_state: Arc<Mutex<State>>) -> Result<()> {
     while let Some(msg) = stream.next().await {
-        let opt_dir = match msg?.payload {
-            InputEvent::Key(KeyEvent { code, down, .. }) if down => match code.as_str() {
-                "ArrowLeft" => Some(Delta::LEFT),
-                "ArrowUp" => Some(Delta::UP),
-                "ArrowRight" => Some(Delta::RIGHT),
-                "ArrowDown" => Some(Delta::DOWN),
-                _ => None,
-            }
-            InputEvent::Gamepad(GamepadEvent { control, .. }) => match control {
-                GamepadControlEvent::Button(GamepadButtonEvent { index, down, .. }) if down => match index {
-                    // Per https://w3c.github.io/gamepad/#remapping
-                    12 => Some(Delta::UP),
-                    13 => Some(Delta::DOWN),
-                    14 => Some(Delta::LEFT),
-                    15 => Some(Delta::RIGHT),
-                    _ => None,
-                },
-                _ => None,
-            }
-            _ => None,
-        };
-
+        let input_event = msg?.payload;
         // Update the snake's direction
-        if let Some(dir) = opt_dir {
+        if let Some(dir) = input_event.direction() {
             debug!("Rotating snake head to {:?}", dir);
             let mut state = shared_state.lock().await;
-            state.snake.rotate_head(dir);
+            state.snake.rotate_head(dir.into());
         }
     }
 
